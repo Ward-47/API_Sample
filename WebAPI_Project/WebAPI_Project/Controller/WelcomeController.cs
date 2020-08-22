e57558b83d59843791176bba4286765ba25cb6bd
Ward-47 <waleavines@yahoo.com>
2020-08-22 15:29:26 -0500

    When you open it on a webpage, go to url and add /api/welcome.

---
+++ WebAPI_Project/WebAPI_Project/Controller/WelcomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace WebAPI_Project.Controllers
+{
+    public class WelcomeController : ApiController // we use ApiController
+    {
+       public string Get()
+        {
+            string strWelcome = "Halo Infinite is going to be awesome!";
+            return strWelcome;
+        }
+    }
+}
